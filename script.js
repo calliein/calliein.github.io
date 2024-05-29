66c01b75e6115e7ec961ccc0f6a8895889fca85f
calliein <140846329+calliein@users.noreply.github.com>
2024-05-29 13:55:19 -0400

    Remove modals

--- script.js
+++ script.js
@@ -40,29 +40,3 @@ function openPage(evt, pageName) {
 
 // Start with "About" tab open
 document.getElementById("defaultOpen").click();
-
-// Set up modals
-const openEls = document.querySelectorAll("[data-open]");
-const isVisible = "is-visible";
-for (const el of openEls) {
-    el.addEventListener("click", function () {
-        const modalId = this.dataset.open;
-        document.getElementById(modalId).classList.add(isVisible);
-    });
-}
-const closeEls = document.querySelectorAll("[data-close]");
-for (const el of closeEls) {
-    el.addEventListener("click", function () {
-        this.parentElement.parentElement.parentElement.classList.remove(isVisible);
-    });
-}
-document.addEventListener("click", e => {
-    if (e.target == document.querySelector(".modal.is-visible")) {
-        document.querySelector(".modal.is-visible").classList.remove(isVisible);
-    }
-});
-document.addEventListener("keyup", e => {
-    if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
-        document.querySelector(".modal.is-visible").classList.remove(isVisible);
-    }
-});
